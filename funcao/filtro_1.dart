43acfb47b0f7811ec7ae06d5416d971fa583c7e8
Felipe Gueller <felipe.gueller@gmail.com>
2020-07-27 18:27:56 -0300

    Aplicando mais um exemplo da função where no arquivo filtro_1.dart

--- funcao/filtro_1.dart
+++ funcao/filtro_1.dart
@@ -1,5 +1,5 @@
 main(List<String> args) {
-  var notas = [6.7, 3.4, 8.9, 7.4, 9.3, 2.5, 7.9];
+  var notas = [6.7, 3.4, 8.9, 7.4, 9.3, 2.5, 7.9, 5.4];
   /* Filtro para a nova lista receber apenas as notas maiores que 7(nota >= 7.0) */
 
   var notasBoas = [];
@@ -10,7 +10,7 @@ main(List<String> args) {
     }
   }
   print(notasBoas);
-  
+
   var notasMuitoBoas = [];
 
   for (double nota in notas) {
@@ -20,5 +20,8 @@ main(List<String> args) {
   }
   print(notasMuitoBoas);
 
-  
+
+  var recuperacao = (double nota) => nota >= 5.0 && nota < 7.0;
+  var notasRecuperacao = notas.where(recuperacao);
+  print(notasRecuperacao);
 }
