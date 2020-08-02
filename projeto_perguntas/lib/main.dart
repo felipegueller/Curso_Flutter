35bf4835090a80afb234bd8fc197613642383a88
Felipe Gueller <felipe.gueller@gmail.com>
2020-08-01 22:32:37 -0300

    Atualizando o projeto_perguntas

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -8,6 +8,7 @@ main(List<String> args) => runApp(new PerguntaApp());
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
 
+  // altera o indice para trocar de pergunta
   void _responder() {
     setState(() {
       _perguntaSelecionada++;
@@ -16,10 +17,27 @@ class _PerguntaAppState extends State<PerguntaApp> {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> perguntas = [
-      "Qual é a sua cor favorita?",
-      "Qual é o seu animal favorito?",
+    final perguntas = [
+      {
+        'texto': "Qual é a sua cor favorita?",
+        'respostas': ["Verde", "Amarelo", "Vermelho", "Preto"],
+      },
+      {
+        'texto': "Qual é o seu animal favorito?",
+        'respostas': ["Cavalo", "Porquinho da índia", "Coelho", "Aveztruz"],
+      },
+      {
+        'texto': "Qual é a sua marca de chocolates favorita?",
+        'respostas': ["Cacau Show", "Garoto", "Nestle", "Arcor"],
+      },
     ];
+
+    List<String> respostas = perguntas[_perguntaSelecionada]['respostas'];
+    // List<Widget> widgets =
+    // Adciona cada respostas do da lista deperguntas na lista de respostas
+    // for (String textoResposta in respostas) {
+    //   widgets.add(Resposta(textoResposta, _responder));
+    // }
 
     return MaterialApp(
       home: Scaffold(
@@ -28,10 +46,12 @@ class _PerguntaAppState extends State<PerguntaApp> {
         ),
         body: Column(
           children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]),
-            Resposta("Resposta 1", _responder),
-            Resposta("Resposta 2", _responder),
-            Resposta("Resposta 3", _responder),
+            Questao(perguntas[_perguntaSelecionada]['texto']),
+
+            /* Pegou a lista de Strings, transformou para uma lista de Widgets e depois o resultado do map em uma lista
+             depois o spread colocou la lista de widget acima em children */
+            ...respostas.map((texto) => Resposta(texto, _responder)).toList(),
+            // operador spread (...) adiciona todos os valores da lista em outra lista
           ],
         ),
       ),
