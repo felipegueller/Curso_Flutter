639a0b49aa121f791d7b7988c38545be27159363
Felipe Gueller <felipe.gueller@gmail.com>
2020-08-12 22:19:35 -0300

    Refatorando a classe main.dart

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -7,6 +7,20 @@ main(List<String> args) => runApp(new PerguntaApp());
 // dentro do genérics passamos qual o componente stateful que a gente deseja controlar
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  final _perguntas = [
+    {
+      'texto': "Qual é a sua cor favorita?",
+      'respostas': ["Verde", "Amarelo", "Vermelho", "Preto"],
+    },
+    {
+      'texto': "Qual é o seu animal favorito?",
+      'respostas': ["Cavalo", "Porquinho da índia", "Coelho", "Aveztruz"],
+    },
+    {
+      'texto': "Qual é a sua marca de chocolates favorita?",
+      'respostas': ["Cacau Show", "Garoto", "Nestle", "Arcor"],
+    },
+  ];
 
   // altera o indice para trocar de pergunta
   void _responder() {
@@ -15,45 +29,40 @@ class _PerguntaAppState extends State<PerguntaApp> {
     });
   }
 
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      {
-        'texto': "Qual é a sua cor favorita?",
-        'respostas': ["Verde", "Amarelo", "Vermelho", "Preto"],
-      },
-      {
-        'texto': "Qual é o seu animal favorito?",
-        'respostas': ["Cavalo", "Porquinho da índia", "Coelho", "Aveztruz"],
-      },
-      {
-        'texto': "Qual é a sua marca de chocolates favorita?",
-        'respostas': ["Cacau Show", "Garoto", "Nestle", "Arcor"],
-      },
-    ];
-
-    List<String> respostas = perguntas[_perguntaSelecionada]['respostas'];
-    // List<Widget> widgets =
-    // Adciona cada respostas do da lista deperguntas na lista de respostas
-    // for (String textoResposta in respostas) {
-    //   widgets.add(Resposta(textoResposta, _responder));
-    // }
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada]['respostas']
+        : null;
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Perguntas"),
         ),
-        body: Column(
-          children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]['texto']),
+        body: temPerguntaSelecionada // operador ternério
+            ? Column(
+                children: <Widget>[
+                  Questao(_perguntas[_perguntaSelecionada]['texto']),
 
-            /* Pegou a lista de Strings, transformou para uma lista de Widgets e depois o resultado do map em uma lista
+                  /* Pegou a lista de Strings, transformou para uma lista de Widgets e depois o resultado do map em uma lista
              depois o spread colocou la lista de widget acima em children */
-            ...respostas.map((texto) => Resposta(texto, _responder)).toList(),
-            // operador spread (...) adiciona todos os valores da lista em outra lista
-          ],
-        ),
+                  ...respostas
+                      .map((texto) => Resposta(texto, _responder))
+                      .toList(),
+                  // operador spread (...) adiciona todos os valores da lista em outra lista
+                ],
+              )
+            : Center(
+                child: Text(
+                  'Parabéns!',
+                  style: TextStyle(fontSize: 28),
+                ),
+              ), // fim da operação ternária
       ),
     );
   }
