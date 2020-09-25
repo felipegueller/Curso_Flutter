624a1fdc0270a37fb9c3ce62bfcb79b641726ab8
Felipe Gueller <felipe.gueller@gmail.com>
2020-09-25 11:40:08 -0300

    Adicionando o Widget ListView

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -11,64 +11,63 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300, // tamanho do container
-      child: SingleChildScrollView(
-        // (SingleChildScrollView) habilita o scroll(rolagem) da lista
-        child: Column(
-          children: transactions.map((tr) {
-            // children recebe direto uma lista de transaction
-            // tr -> represensa uma instância de transaction
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      // pesquisar sobre
-                      horizontal: 15,
-                      vertical: 10,
+      // O ListView precisa ter um componente pai para definir o scroll
+      child: ListView.builder(
+        itemCount: transactions.length, // Passando o tamanho da lista
+        itemBuilder: (ctx, index) {
+          // (itemBuilder)Só chama isso no momento que ele for precisando da informação, renderizar a lista toda de uma vez, o que atrapalha a performance do App
+          final tr = transactions[index]; // recebe objeto transaction
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    // pesquisar sobre
+                    horizontal: 15,
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    // (BoxDecoration) Widget que permite mexer com a borda do container
+                    border: Border.all(
+                      color: Colors.purple, // cor da borda
+                      width: 2, // largura da borda
                     ),
-                    decoration: BoxDecoration(
-                      // (BoxDecoration) Widget que permite mexer com a borda do container
-                      border: Border.all(
-                        color: Colors.purple, // cor da borda
-                        width: 2, // largura da borda
-                      ),
+                  ),
+                  padding:
+                      EdgeInsets.all(10), // Padding de 10px para as 4 direções
+                  child: Text(
+                    'R\$ ${tr.value.toStringAsFixed(2)}', // toStringAsFixed permite definir a quantidade de casas decimais
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold, // peso da fonte
+                      fontSize: 20, // tamanho da fonte
+                      color: Colors.purple, // cor da fonte
                     ),
-                    padding: EdgeInsets.all(
-                        10), // Padding de 10px para as 4 direções
-                    child: Text(
-                      'R\$ ${tr.value.toStringAsFixed(2)}', // toStringAsFixed permite definir a quantidade de casas decimais
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment
+                      .start, // Eixo cruzado da Column (Eixo x)
+                  children: <Widget>[
+                    Text(
+                      tr.title, // Titulo da transação
                       style: TextStyle(
-                        fontWeight: FontWeight.bold, // peso da fonte
-                        fontSize: 20, // tamanho da fonte
-                        color: Colors.purple, // cor da fonte
+                        fontSize: 16, // Tamanho da fonte
+                        fontWeight: FontWeight.bold, // Peso da fonte
                       ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment
-                        .start, // Eixo cruzado da Column (Eixo x)
-                    children: <Widget>[
-                      Text(
-                        tr.title, // Titulo da transação
-                        style: TextStyle(
-                          fontSize: 16, // Tamanho da fonte
-                          fontWeight: FontWeight.bold, // Peso da fonte
-                        ),
+                    Text(
+                      DateFormat('d MMM y')
+                          .format(tr.date), // Permite a formatação da data
+                      style: TextStyle(
+                        color: Colors.grey, // cor do texto
                       ),
-                      Text(
-                        DateFormat('d MMM y')
-                            .format(tr.date), // Permite a formatação da data
-                        style: TextStyle(
-                          color: Colors.grey, // cor do texto
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
